07ccfa98d36be033a982e2dc8047a8ff3733c23d
Lukas Jecha <lukas.jecha@sinnerschrader.com>
2016-04-24 16:15:31 +0200

    connect state to render

--- src/browser/fileupload/index.js
+++ src/browser/fileupload/index.js
@@ -5,14 +5,15 @@
   'redux-instance'
 ], function(constants, page, Immutable, dux) {
   function getModuleElement() {
-    return document.getElementById('fileupload');
+    return document.getElementById(constants.get('elementId'));
   }
 
   console.log(dux.getState().fileUpload.toObject());
 
   var render = function(state) {
     // Dont do .inerHTML = it is dangerous.
-    getModuleElement().innerHTML = page();
+    var state = dux.getState().fileUpload.toObject();
+    getModuleElement().innerHTML = page(state);
   }
   return {
     render: function() {
